c9fd02ca9b72c05d9a037c037c7ccd0d899139fc
ahllyy <amanda.halley@keyin.com>
2024-10-03 16:07:05 -0230

    Testing for myPoint class

--- Problem1/TestMyLine.java
+++ Problem1/TestMyLine.java
@@ -1,5 +1,17 @@
 package Problem1;
 
 public class TestMyLine {
-    
+    public static void main(String[] args) {
+        MyPoint p1 = new MyPoint(0, 0);
+        MyPoint p2 = new MyPoint(6,8);
+
+        //Using "MyPoint another" method
+        double distance1 = p1.distance(p2);
+        System.out.println();
+        System.out.println("\nDistance between point 1 and point 2 is: " + distance1);
+
+        //using distance (x, y)
+        double distance2 = p1.distance(3, 4);
+        System.out.println("\nDistance between point 1 and (3,4) is: " + distance2);
+    }
 }
