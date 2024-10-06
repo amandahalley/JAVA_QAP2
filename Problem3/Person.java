354c240cfd4ed464480ab610ac92edfa362ed598
ahllyy <amanda.halley@keyin.com>
2024-10-06 17:50:46 -0230

    create basic skeleton

--- Problem3/Person.java
+++ Problem3/Person.java
@@ -1,5 +1,19 @@
 package Problem3;
 
 public class Person {
-    
+    private String lastName;
+    private String firstName;
+    private Address home;
+
+
+    public Person(String lastName, String firstName, Address home) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.home = home;
+    }
+
+
+    Public String toString() {
+        return "";
+    }
 }
