3cbbd377abab6a1b1765bd6fc770020429b8c727
ahllyy <amanda.halley@keyin.com>
2024-10-06 14:16:59 -0230

    Testing for points of rectangle  & width/length

--- Problem2/TestMyRectangle.java
+++ Problem2/TestMyRectangle.java
@@ -1,5 +1,17 @@
 package Problem2;
+import Problem1.MyPoint;
 
 public class TestMyRectangle {
-    
+    public static void main(String[] args) {
+        MyPoint topLeft = new MyPoint(3,7);
+        MyPoint bottomRight = new MyPoint (9,4);
+
+
+        MyRectangle rectangle = new MyRectangle(topLeft, bottomRight);
+
+        //Test basic methods of MyRectangle class
+
+        System.out.println("My Rectangle: " + rectangle);
+
+    }
 }
