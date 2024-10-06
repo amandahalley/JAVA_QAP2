c7ee9baab0be2d809a93dce56bb9beb9af20d532
ahllyy <amanda.halley@keyin.com>
2024-10-06 17:29:43 -0230

    Initial commit, basic skeleton

---
+++ Problem3/Money.java
@@ -0,0 +1,34 @@
+package Problem3;
+
+public class Money {
+    private long dollars;
+    private long cents;
+
+   public Money (double amount) {
+
+   }
+
+   public Money (Money otherObject) {
+
+   }
+
+   public Money add(Money otherAmount) {
+
+   }
+
+   public Money subtract(Money otherAmount) {
+
+   }
+
+   public int compareTo(Money otherObject) {
+
+   }
+
+   public boolean equals(Money otherObject) {
+
+   }
+
+   public String toString() {
+    return "";
+   }
+}
