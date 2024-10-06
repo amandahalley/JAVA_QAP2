1624d33fb60e385da59aafb5168061a49ae7a8a1
ahllyy <amanda.halley@keyin.com>
2024-10-06 17:51:20 -0230

    started skeleton

--- Problem3/CreditCard.java
+++ Problem3/CreditCard.java
@@ -1,5 +1,14 @@
 package Problem3;
 
 public class CreditCard {
-    
+    private Money balance;
+    private Money creditLimit;
+    private Person owner;
+
+    public CreditCard(Person newCardHolder, Money limit) {
+        this.owner = newCardHolder;
+        this.creditLimit = new Money(limit); //Uses copy constructor
+        this.balance = new Money (0); //initialize balance to be 0
+
+        
 }
